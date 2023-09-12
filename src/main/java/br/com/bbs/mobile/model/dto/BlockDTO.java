84d841ef4caf3b4338ffc1aa31f3d092bbb2e46c
Pedro Appel <appelpedroappel@gmail.com>
2023-09-12 20:40:46 -0300

    fix: enhancing Block DTO with Lombok

--- src/main/java/br/com/bbs/mobile/model/dto/BlockDTO.java
+++ src/main/java/br/com/bbs/mobile/model/dto/BlockDTO.java
@@ -1,27 +1,40 @@
 package br.com.bbs.mobile.model.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonSerialize()
 public class BlockDTO {
+
     private String medicine;
     private String patientKey;
     private String doctorKey;
-    private String creationDate;
-    private String expirationDate;
     private String signature;
+    private LocalDate creationDate;
+    private LocalDate expirationDate;
 
-    public BlockDTO(String medicine, String patientKey, String doctorKey, String creationDate, String expirationDate, String signature) {
-        this.medicine = medicine;
-        this.patientKey = patientKey;
-        this.doctorKey = doctorKey;
-        this.creationDate = creationDate;
-        this.expirationDate = expirationDate;
-        this.signature = signature;
+    @Override
+    public String toString() {
+        return "[" +
+                "medicine='" + getSubstring(medicine) + '\'' +
+                ", patientKey='" + getSubstring(patientKey) + '\'' +
+                ", doctorKey='" + getSubstring(doctorKey)+ '\'' +
+                ", creationDate=" + creationDate +
+                ", expirationDate=" + expirationDate +
+                ", signature='" + getSubstring(signature) + '\'' +
+                ']';
     }
 
+    private String getSubstring(String text) {
+        return text.substring(text.length() - 10);
+    }
 }
