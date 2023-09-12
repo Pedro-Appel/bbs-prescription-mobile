0970d85329d137a20fffba3b48d3e77585400d1e
Pedro Appel <appelpedroappel@gmail.com>
2023-09-12 20:39:34 -0300

    fix: removing interface implementation

--- src/main/java/br/com/bbs/mobile/model/KeyResponse.java
+++ src/main/java/br/com/bbs/mobile/model/KeyResponse.java
@@ -1,12 +1,20 @@
 package br.com.bbs.mobile.model;
 
-import br.com.bbs.mobile.model.Response;
 import lombok.Getter;
 
 @Getter
-public class KeyResponse implements Response {
-    private final String publicKey;
-    public KeyResponse(String publicKey) {
+public class KeyResponse {
+
+    private String publicKey;
+
+    private KeyResponse() {}
+
+    public static KeyResponse KeyResponseBuilder(){
+        return new KeyResponse();
+    }
+
+    public KeyResponse withPublicKey(String publicKey){
         this.publicKey = publicKey;
+        return this;
     }
 }
--- src/main/java/br/com/bbs/mobile/model/VerifyResponse.java
+++ src/main/java/br/com/bbs/mobile/model/VerifyResponse.java
@@ -3,12 +3,19 @@ package br.com.bbs.mobile.model;
 import lombok.Getter;
 
 @Getter
-public class VerifyResponse implements Response {
+public class VerifyResponse {
+
     private final boolean valid;
     private final String data;
+    private String reason;
 
     public VerifyResponse(boolean valid, String plainText) {
         this.valid = valid;
         this.data = plainText;
+    }
+    public VerifyResponse(boolean valid, String plainText, String reason) {
+        this.valid = valid;
+        this.data = plainText;
+        this.reason = reason;
     }
 }
