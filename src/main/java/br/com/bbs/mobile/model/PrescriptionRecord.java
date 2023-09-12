4ad91d8f1e524aa7908de4b39afb024cd1d6d2e2
Pedro Appel <appelpedroappel@gmail.com>
2023-09-12 20:38:41 -0300

    feat: removing useless class and changing DTO to Record for better maintance

---
+++ src/main/java/br/com/bbs/mobile/model/PrescriptionRecord.java
@@ -0,0 +1,4 @@
+package br.com.bbs.mobile.model;
+
+public record PrescriptionRecord(String medicine, int expirationInDays) {
+}
--- src/main/java/br/com/bbs/mobile/model/Response.java
+++
@@ -1,7 +0,0 @@
-package br.com.bbs.mobile.model;
-
-import java.io.Serializable;
-
-public interface Response {
-
-}
