febb9e301f2009fe8d2cd1a63e1317db8cd382a7
Pedro Appel <appelpedroappel@gmail.com>
2023-09-12 20:40:01 -0300

    feat: including personalized exceptions to handle later

---
+++ src/main/java/br/com/bbs/mobile/exception/ClientKeyException.java
@@ -0,0 +1,7 @@
+package br.com.bbs.mobile.exception;
+
+public class ClientKeyException extends RuntimeException {
+    public ClientKeyException(Exception e) {
+        super(e);
+    }
+}
---
+++ src/main/java/br/com/bbs/mobile/exception/DoctorException.java
@@ -0,0 +1,7 @@
+package br.com.bbs.mobile.exception;
+
+public class DoctorException extends RuntimeException {
+    public DoctorException(Exception e) {
+        super(e);
+    }
+}
